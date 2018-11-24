9eb1aee9965252ae26188ccc2d7c6836a85c547d
Martin de Keijzer <martin@spinninrecords.nl>
2018-11-24 14:53:05 +0100

    Change archives title to a static name

--- archive-sessions.php
+++ archive-sessions.php
@@ -5,8 +5,8 @@ get_header(); ?>
 
         <?php if ( have_posts() ) : ?>
             <header class="page-header">
+                <h1 class="page-title">Sessions</h1>
                 <?php
-                the_archive_title( '<h1 class="page-title">', '</h1>' );
                 the_archive_description( '<div class="taxonomy-description">', '</div>' );
                 ?>
             </header><!-- .page-header -->
