feea0ac3f4da68f612990dd2ed1b63e8c7a129fd
Martin de Keijzer <martin@spinninrecords.nl>
2018-12-19 09:47:08 +0100

    Change ACF ordering for talks and sessions

--- functions.php
+++ functions.php
@@ -1,15 +1,12 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'phpbnl18_enqueue_styles' );
-add_action( 'pre_get_posts', 'phpbnl_change_sort_order');
-
 /**
  * Set parent theme styles
  */
 function phpbnl18_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
 }
+add_action( 'wp_enqueue_scripts', 'phpbnl18_enqueue_styles' );
 
 /**
  * Change sort order to title on archive pages
@@ -17,12 +14,15 @@ function phpbnl18_enqueue_styles() {
  */
 function phpbnl_change_sort_order($query){
     if(is_archive() && isset($query->query_vars['post_type']) && $query->query_vars['post_type'] == 'sessions'):
+        $query->set( 'orderby', 'meta_value');
+        $query->set( 'meta_key', 'session_type');
         $query->set('order', 'ASC');
-        $query->set( 'orderby', 'session_type');
     elseif (is_archive() && isset($query->query_vars['post_type']) && $query->query_vars['post_type'] == 'speaker'):
+        $query->set( 'orderby', 'meta_value' );
+        $query->set( 'meta_key', 'title' );
         $query->set( 'order', 'ASC' );
-        $query->set( 'orderby', 'title' );
     endif;
 
     return $query;
 };
+add_action( 'pre_get_posts', 'phpbnl_change_sort_order');
